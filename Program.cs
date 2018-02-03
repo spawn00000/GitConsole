fbe3ce847c15c4d3554fbfbcc1be5d87ffea6163
Marius <avramescu.marius@gmail.com>
2018-02-03 12:52:25 +0200

    read from file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace GitConsole
 {
@@ -9,6 +10,36 @@ namespace GitConsole
     {
         static void Main(string[] args)
         {
+            //inits 
+           
+            //the file is txt and is in debug folder
+            //separated by comma and space
+            string fileName = "AddressBook.txt";
+            string[] separators = new string[1];
+            separators[0] = ", ";
+
+            List<string> firstName = new List<string>();
+
+
+            //read from file 
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {                    
+
+                    string[] el = sr.ReadLine().Split(separators,StringSplitOptions.None);
+                    //el[1] = name
+                    //el[2] = sex
+                    //el[3] = date of birth?
+
+                    string[] name = el[0].Split(' ');
+                    string first = name[0]; firstName.Add(first);
+                    string last = name[1];
+
+                }
+                sr.Close();
+            }
+
 
             string[] answers = new string[3];
 
