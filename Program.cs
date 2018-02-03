6e74bc072a8af78dddbb97e18c2d938f734f5525
Marius <avramescu.marius@gmail.com>
2018-02-03 12:20:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] answers = new string[3];
+
+            //print results
+            for (int i = 0; i < answers.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + answers[i]);
+            }
+
+
+            //keep console open.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
